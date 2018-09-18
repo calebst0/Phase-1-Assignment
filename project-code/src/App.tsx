24561529a6c7e82da4ac090461b695e649f51f64
calebst0 <41213856+calebst0@users.noreply.github.com>
2018-09-19 10:45:07 +1200

    change

--- project-code/src/App.tsx
+++ project-code/src/App.tsx
@@ -6,7 +6,7 @@ class App extends React.Component {
 
     return (
       <div className='centreText'>
-        This is a GrandExchange tracker site.
+        ahusdahsdhufasuifduahs.
       </div>
     );
   }
